0a0ae080fe5a6510d886b502dcea47f5d9a945cd
vestacke-neuronske-mreze <119251043+vestacke-neuronske-mreze@users.noreply.github.com>
2022-11-27 21:11:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from test_examples.basic_rnn import test_basic_RNN
+from test_examples.binary_classification import test_binary_classification
+from test_examples.convolution2D import test_CNN
+from test_examples.lstm_gru import test_LSTM_GRU
+from test_examples.multiclass_classification import test_multiclass_classification, UseDataset
+from test_examples.regression import test_regression
+from test_examples.test_gan import test_GAN
+from test_examples.test_vae import test_VAE
+
+if __name__ == '__main__':
+    # test_regression()
+    # test_binary_classification()
+    # test_multiclass_classification(UseDataset.WINE)
+    # test_CNN()
+    # test_basic_RNN()
+    # test_LSTM_GRU()
+    # test_VAE()
+    # test_GAN()
+    pass
+
